8a33427972455483dd66aa5c56d735f0802de3f6
Vshvalogan <loganathan.vshvapoorani@gmail.com>
2025-11-23 13:39:00 +0800

    feat: Add Policy image to each policies

--- frontend/src/components/PolicyCard.jsx
+++ frontend/src/components/PolicyCard.jsx
@@ -1,15 +1,39 @@
 // src/components/PolicyCard.jsx
 import { Link } from "react-router-dom";
 
-export default function PolicyCard({ policy, to }) {
+export default function PolicyCard({ policy }) {
+  const imgSrc = policy.image_url || policy.image ;
+
   return (
-    <div>
-      <h3>{policy.name}</h3>
-      <p>Type: {policy.type}</p>
-      <p>Premium: {policy.premium}</p>
-      <p>Coverage: {policy.coverage_amount}</p>
-      <p>{policy.description}</p>
-      {to && <Link to={to}>View details</Link>}
+    <div className="policy-card">
+      <div className="policy-image">
+        <img src={imgSrc} alt={policy.name} />
+      </div>
+
+      <div className="policy-content">
+        <h3 className="policy-title">{policy.name}</h3>
+
+        <p className="policy-desc">
+          {policy.description?.slice(0, 120) ||
+            "Comprehensive insurance coverage."}
+        </p>
+
+        <ul className="policy-points">
+          <li>✔ Coverage amount: ${policy.coverage_amount}</li>
+          <li>✔ Premium: ${policy.premium}/month</li>
+          <li>✔ Type: {policy.type}</li>
+        </ul>
+
+        <div className="policy-actions">
+          <Link className="learn-btn" to={`${policy.id}`}>
+            Learn more →
+          </Link>
+          <p></p>
+          <Link className="buy-btn" to={`apply/${policy.id}`}>
+            BUY NOW
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
